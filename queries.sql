0c120ce4357ed7786381e9a10eacbe525dac6f5e
JoseCarrilloURU <jose.30739823@uru.edu>
2025-04-05 04:51:18 -0400

    DB Reset

--- queries.sql
+++ queries.sql
@@ -1,2 +1,7 @@
--- Remove the queryParams column from the logs table
-ALTER TABLE logs DROP COLUMN queryParams;
+-- Delete all data from the logs table and reset its ID
+DELETE FROM logs;
+DELETE FROM sqlite_sequence WHERE name = 'logs';
+
+-- Delete all data from the userLogs table and reset its ID
+DELETE FROM userLogs;
+DELETE FROM sqlite_sequence WHERE name = 'userLogs';
